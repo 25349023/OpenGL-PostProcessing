21f90782ec6ff236ce8dac41624f8641c4c41df6
sky9023 <25349023.qq@gmail.com>
2022-11-22 20:37:32 +0800

    [UPDATE] load model data, textures and construct buffers

--- Assets/fragment.fs.glsl
+++ Assets/fragment.fs.glsl
@@ -1,6 +1,6 @@
 #version 430
 
-layout(location = 0) out vec4 fragColor;
+layout (location = 0) out vec4 fragColor;
 
 uniform mat4 um4mv;
 uniform mat4 um4p;
@@ -17,6 +17,6 @@ uniform sampler2D tex;
 
 void main()
 {
-    vec3 texColor = texture(tex,vertexData.texcoord).rgb;
+    vec3 texColor = texture(tex, vertexData.texcoord).rgb;
     fragColor = vec4(texColor, 1.0);
 }
--- Source/main.cpp
+++ Source/main.cpp
@@ -8,8 +8,113 @@ GLubyte timer_cnt = 0;
 bool timer_enabled = true;
 unsigned int timer_speed = 16;
 
+const aiScene* scene;
+
+GLuint vao;
+
 using namespace glm;
 using namespace std;
+
+struct Vertex
+{
+    vec3 position{};
+    vec3 normal{};
+    vec2 tex_coords{};
+};
+
+struct Shape
+{
+    GLuint vao;
+    GLuint vbo;
+    GLuint ibo;
+
+    vector<Vertex> vertices;
+    vector<unsigned int> indices;
+
+    int drawCount;
+    int materialID;
+
+    Shape(): vao(0), vbo(0), ibo(0),
+             drawCount(0), materialID(0) {}
+
+    void extractMeshData(const aiMesh* mesh);
+    void extractMeshIndices(const aiMesh* mesh);
+    void bindBuffers();
+};
+
+void Shape::extractMeshData(const aiMesh* mesh)
+{
+    for (unsigned int v = 0; v < mesh->mNumVertices; ++v)
+    {
+        Vertex vertex;
+        vertex.position = vec3(mesh->mVertices[v].x, mesh->mVertices[v].y, mesh->mVertices[v].z);
+        vertex.normal = vec3(mesh->mNormals[v].x, mesh->mNormals[v].y, mesh->mNormals[v].z);
+        if (mesh->HasTextureCoords(0))
+        {
+            vertex.tex_coords = vec2(mesh->mTextureCoords[0][v].x, mesh->mTextureCoords[0][v].y);
+        }
+        vertices.push_back(vertex);
+    }
+}
+
+void Shape::extractMeshIndices(const aiMesh* mesh)
+{
+    for (unsigned int f = 0; f < mesh->mNumFaces; ++f)
+    {
+        unsigned int* idx = mesh->mFaces[f].mIndices;
+        indices.insert(indices.end(), idx, idx + 3);
+    }
+}
+
+void Shape::bindBuffers()
+{
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // vertex buffer
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, tex_coords));
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+
+    // index buffer
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), indices.data(), GL_STATIC_DRAW);
+
+    glBindVertexArray(0);
+}
+
+struct Material
+{
+    GLuint diffuse_tex{};
+
+    void bindTexture(texture_data texture);
+};
+
+void Material::bindTexture(texture_data texture)
+{
+    glGenTextures(1, &diffuse_tex);
+    glBindTexture(GL_TEXTURE_2D, diffuse_tex);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, texture.width, texture.height, 0,
+                 GL_RGBA, GL_UNSIGNED_BYTE, texture.data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
+
+vector<Shape> shapes;
+vector<Material> materials;
 
 char** loadShaderSource(const char* file)
 {
@@ -31,12 +136,72 @@ void freeShaderSource(char** srcp)
     delete[] srcp;
 }
 
+void loadGeometry()
+{
+    for (unsigned int i = 0; i < scene->mNumMeshes; ++i)
+    {
+        const aiMesh* mesh = scene->mMeshes[i];
+        Shape shape;
+        
+        shape.extractMeshData(mesh);
+        shape.extractMeshIndices(mesh);
+
+        shape.bindBuffers();
+
+        shape.materialID = mesh->mMaterialIndex;
+        shape.drawCount = mesh->mNumFaces * 3;
+
+        shapes.push_back(shape);
+    }
+}
+
+void loadMaterials()
+{
+    for (unsigned int i = 0; i < scene->mNumMaterials; ++i)
+    {
+        aiMaterial* ai_material = scene->mMaterials[i];
+        Material material;
+        aiString texture_path;
+        if (ai_material->GetTexture(aiTextureType_DIFFUSE, 0, &texture_path) == aiReturn_SUCCESS)
+        {
+            char full_path[64] = "../Objects/";
+            strncat(full_path, texture_path.C_Str(), 50);
+            
+            texture_data texture = loadImg(full_path);
+            material.bindTexture(texture);
+        }
+        // else
+        // {
+            // load some default image as default_diffuse_tex
+            // Material.diffuse_tex = default_diffuse_tex;
+        // }
+
+        materials.push_back(material);
+    }
+}
+
 void My_Init()
 {
     glClearColor(0.0f, 0.6f, 0.0f, 1.0f);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
 
+    scene = aiImportFile(
+        "../Objects/sibenik.obj",
+        aiProcess_GenNormals |
+        aiProcess_CalcTangentSpace |
+        aiProcess_Triangulate |
+        aiProcess_JoinIdenticalVertices |
+        aiProcess_SortByPType
+    );
+
+    cout << "There are " << scene->mNumMeshes << " meshes." << endl;
+    cout << "There are " << scene->mNumMaterials << " materials." << endl;
+
+    loadGeometry();
+    loadMaterials();
+
+    aiReleaseImport(scene);
 }
 
 void My_Display()
